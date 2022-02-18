b8f6a0487c6234e06eba01fed19276af17fffb4e
pranaysagar1510 <pranaysagar1510@gmail.com>
2022-02-18 14:49:42 +0530

    Authorization

---
+++ Retail_Product_Management/AuthorizationAPI/Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using AuthorizationAPI.Repository;
+using AuthorizationAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization.Controllers
+{
+
+
+    [Authorize]
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository userRepository;
+
+        public UsersController(IUserRepository _userRepository)
+        {
+            userRepository = _userRepository;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserModel cust)
+        {
+            var user = userRepository.Authenticate(cust.username, cust.password);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(user);
+        }
+        /*[AllowAnonymous]
+         [HttpPost("authenticate")]*/
+        [HttpPost]
+        public IActionResult Authenticate(string username, string password)
+        {
+
+            var user = userRepository.Authenticate(username, password);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(user);
+        }
+        [HttpGet("GetUserDetails")]
+        public IActionResult GetUserDetails(string username)
+        {
+            try
+            {
+                UserModel result = userRepository.UserDetails(username);
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                return NotFound();
+            }
+
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("Register/{userid}/{username}/{email}/{password}/{address}")]
+        public IActionResult RegisterR(string userid, string username, string email, string password, string address)
+        {
+            Register register = userRepository.RegisterDetails(userid, username, email, password, address);
+            try
+            {
+                if (register == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(register);
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Retail_Product_Management/AuthorizationAPI/Repository/UserRepository.cs
@@ -0,0 +1,105 @@
+﻿using AuthorizationAPI.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthorizationAPI.Repository
+{
+    public class UserRepository:IUserRepository
+    {
+        
+            private readonly Appsettings _appSettings;
+
+            private readonly string Key;
+            public UserRepository(string Key)
+            {
+                this.Key = Key;
+            }
+            public List<UserModel> userlist = new List<UserModel>() {
+           new UserModel{ userid=1,password="123",username="Bharath"},
+            new UserModel{ userid=2,password="123",username="Anupama"},
+            new UserModel{ userid=3,password="123",username="Renuka"},
+            new UserModel{ userid=4,password="123",username="Nancy"},
+            new UserModel{ userid=5,password="123",username="Pranay"}
+        };
+
+            public UserRepository(IOptions<Appsettings> appSettings)
+            {
+                _appSettings = appSettings.Value;
+            }
+
+            UserModel IUserRepository.Authenticate(string UserName, string password)
+            {
+                var user = userlist.SingleOrDefault(x => x.username == UserName && x.password == password);
+                if (user == null)
+                {
+                    return null;
+                }
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var k = "This is my jwt authentication demo";
+                var key = Encoding.ASCII.GetBytes(k);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.UserData, user.username)
+                }),
+                    /* notBefore: DateTime.UtcNow,*/
+                    Expires = DateTime.UtcNow.AddMinutes(60),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                user.Token = tokenHandler.WriteToken(token);
+                user.password = "";
+                return user;
+            }
+
+            bool IUserRepository.isUniqueUser(string UserName)
+            {
+                var user = userlist.SingleOrDefault(x => x.username == UserName);
+                if (user == null)
+                {
+                    return true;
+                }
+                return false;
+            }
+            public UserModel UserDetails(string username)
+            {
+
+                foreach (var user in userlist)
+                {
+                    if (user.username == username)
+                    {
+                        return user;
+                    }
+                }
+                return null;
+
+            }
+
+            Register IUserRepository.RegisterDetails(string userid, string username, string Email, string password, string address)
+            {
+                Register register = new Register()
+                {
+                    userid = Guid.NewGuid().ToString(),
+                    username = username,
+                    Email = Email,
+                    password = password,
+                    address = address
+                };
+
+                return register;
+
+
+
+            }
+
+
+        }
+    }
+
